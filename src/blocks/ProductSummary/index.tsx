a8b17cf10c96eecd0a686ab59e9e549b0f80da30
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2022-06-30 09:14:56 -0300

    feat: add products infos

--- src/blocks/ProductSummary/index.tsx
+++ src/blocks/ProductSummary/index.tsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
 
+import styles from './ProductSummary.module.scss';
+
 import products from 'data/products.json';
 import NotFound from "blocks/NotFound";
+import classNames from "classnames";
 
 export default function ProductSummary() {
   const params = useParams();
@@ -11,11 +14,30 @@ export default function ProductSummary() {
   if (!item) return <NotFound />
 
   return (
-    <>
-      <br/>
-      <br/>
-      <br/>
-      <h1>{item?.title}</h1>
-    </>
+    <div className={classNames({
+      'container': true,
+      [styles['product-summary']]: true
+    })}>
+      <div className='row align-items-start'>
+        <div className={classNames({
+          'col': true,
+          [styles['product-summary__image-container']]: true
+        })}>
+          <img src={item?.image} alt="Product image" />
+        </div>
+
+        <div className={classNames({
+          'col': true,
+          [styles['product-summary__details-container']]: true
+        })}>
+          <p>{item?.category}</p>
+          <h2>{item?.title}</h2>
+          <span>{item?.rating.rate}</span>
+          <p>R$ {item?.price}</p>
+          <button>Comprar</button>
+          <p>{item?.description}</p>
+        </div>
+      </div>
+    </div>
   );
 }
