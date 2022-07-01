0d48b2c7c6f22876218d4ca73604f2747e42aaca
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2022-07-01 08:57:33 -0300

    feat: using axios to get Item

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import NotFound from "blocks/NotFound";
 import ProductSummary from "blocks/ProductSummary";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IItem } from "types";
 
 export default function Produto() {
-  return <ProductSummary />;
+  const params = useParams();
+  const [item, setItem] = useState<IItem>();
+
+  useEffect(() => {
+    const { id } = params;
+  
+    axios.get<IItem>(`https://fakestoreapi.com/products/${id}`)
+      .then(response => {
+        setItem(response.data);
+      });
+  }, [params]);
+
+  if (!item) return <NotFound />;
+  
+  return <ProductSummary {...item} />;
 }
